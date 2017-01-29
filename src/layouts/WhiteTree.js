38bb0dedde16a84d4d47ce5a7d2472e4fcc12121
Chris Heninger <chrish@synapsestudios.com>
2017-01-29 13:07:41 -0700

    ⚔

--- src/layouts/WhiteTree.js
+++ src/layouts/WhiteTree.js
@@ -2,67 +2,7 @@ import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import Text from '../components/text/Text';
 
-const defaultCharacterShape = {
-  general: {
-    name: '',
-    class: '',
-    race: '',
-    align: '',
-    deity: '',
-    size: '',
-    space: '',
-    reach: '',
-  },
-  physical: {
-    gender: '',
-    height: '',
-    weight: '',
-    age: '',
-    hair: '',
-    eyes: '',
-    skin: '',
-    misc: '',
-  },
-  points: {
-    hp: '',
-    gp: '',
-    level: '',
-    xp: '',
-    notes: '',
-  },
-  abilities: {
-    str: '',
-    dex: '',
-    con: '',
-    int: '',
-    wis: '',
-    cha: '',
-  },
-  attack: {
-    melee: '',
-    range: '',
-    bab: '',
-    cmb: '',
-    conc: '',
-    init: '',
-  },
-  defense: {
-    ac: '',
-    touch: '',
-    cmd: '',
-    fort: '',
-    ref: '',
-    will: '',
-  },
-  equipment: {
-    'weapon 1': '',
-    'weapon 2': '',
-    'armor 1': '',
-    'armor 2': '',
-    'misc 1': '',
-    'misc 2': '',
-  },
-};
+import { defaultCharacterShape } from '../data';
 
 class WhiteTree extends Component {
   constructor(props) {
